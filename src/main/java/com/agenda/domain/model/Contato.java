dbf68e722caae2573e6bd0b97dcb33e154126eac
victor souza <victor.ti.souza@gmail.com>
2021-02-24 10:11:41 -0300

    refactor contato

--- src/main/java/com/agenda/domain/model/Contato.java
+++ src/main/java/com/agenda/domain/model/Contato.java
@@ -6,31 +6,23 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @EqualsAndHashCode(of = {"id"})
+@Data
 public class Contato {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Getter
-	@Setter
 	private Long id;
-	@Getter
-	@Setter
 	@NotBlank
 	private String nome;
-	@Getter
-	@Setter
 	private String email;
-	@Getter
-	@Setter
 	private String telefone1;
-	@Getter
-	@Setter
 	private String telefone2;
 	
 	
--- src/main/java/com/agenda/domain/service/ContatoService.java
+++ src/main/java/com/agenda/domain/service/ContatoService.java
@@ -21,7 +21,6 @@ public class ContatoService {
 		}
 		
 		return contatoRepository.save(contato);
-		
 	}
 	
 }
